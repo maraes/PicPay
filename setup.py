cf74b5e4e4f5bff82e2c30c6270247936286fc14
Gui maraes <46168501+maraes@users.noreply.github.com>
2022-11-19 10:06:05 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+
+import pkg_resources
+from setuptools import setup, find_packages
+
+setup(
+    
+    url="https://github.com/maraes/PicPay
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
+    include_package_data=True,
+    
+)
